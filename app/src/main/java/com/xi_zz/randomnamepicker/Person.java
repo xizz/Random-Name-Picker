ca8992d27128847bef1cc34104931cd9a7b2f13a
Xi Wei <xwei@handy.com>
2016-10-18 17:17:48 -0400

    V2. Added some pictures.

---
+++ app/src/main/java/com/xi_zz/randomnamepicker/Person.java
@@ -0,0 +1,11 @@
+package com.xi_zz.randomnamepicker;
+
+public class Person {
+	public String name;
+	public int image;
+
+	public Person(final String name, final int image) {
+		this.name = name;
+		this.image = image;
+	}
+}
