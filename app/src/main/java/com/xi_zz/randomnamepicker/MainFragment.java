7bf28357b38ab6a69a25e56e9d7e6c3400a131fd
Xi Wei <xwei@handy.com>
2017-04-09 16:09:26 -0400

    Save state bug fix.

--- app/src/main/java/com/xi_zz/randomnamepicker/MainFragment.java
+++ app/src/main/java/com/xi_zz/randomnamepicker/MainFragment.java
@@ -2,6 +2,7 @@ package com.xi_zz.randomnamepicker;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -104,6 +105,13 @@ public class MainFragment extends Fragment {
 	}
 
 	@Override
+	public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		if (mCurrentPerson != null)
+			setDisplay(mCurrentPerson);
+	}
+
+	@Override
 	public void onResume() {
 		super.onResume();
 		((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.app_name);
@@ -177,7 +185,7 @@ public class MainFragment extends Fragment {
 		setDisplay(mCurrentPerson);
 	}
 
-	private void setDisplay(Person person) {
+	private void setDisplay(@NonNull Person person) {
 		mNameView.setText(person.name);
 		if (person.photo != null)
 			mImageView.setImageBitmap(Util.byteStringToBitmap(person.photo));
